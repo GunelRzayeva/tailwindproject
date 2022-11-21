2498ea861bca22155ab0ce6e2593c848ec03169b
Gunel <rzayevagun1908@gmail.com>
2022-11-21 21:05:41 +0400

    message

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      width:{
+        "56":"56px"
+      },
+     
+      height:{
+        "500":"500px"
+      }
+    },
+  },
+  plugins: [],
+}
